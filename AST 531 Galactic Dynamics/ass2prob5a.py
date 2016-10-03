f88395fea1932ba15453d63446171969d1366b75
Matt Mechtley <mmechtley@gmail.com>
2016-10-03 16:55:59 -0400

    code cleanup on assignment 2 prob 5

--- AST 531 Galactic Dynamics/ass2prob5a.py
+++ AST 531 Galactic Dynamics/ass2prob5a.py
@@ -2,74 +2,84 @@ import numpy as np
 from numpy.linalg import norm
 from matplotlib import pyplot
 
-nfwDispersion = 200.0 ## km/s
-nfwRadius = 5.0 ## kpc
-cellSize = 0.5 ## kpc
+_nfw_dispersion = 200.0  # km/s
+_nfw_radius = 5.0  # kpc
+cell_size = 0.5  # kpc
 
-cgs_G = 6.674e-8 ## G in cgs (cm^3 g^-1 s^-2). Doesn't matter, factors out.
+cgs_G = 6.674e-8  # G in cgs (cm^3 g^-1 s^-2). Doesn't matter, factors out.
 
-nfwRho = nfwDispersion**2 / (4 * np.pi * cgs_G * nfwRadius**2) 
+nfw_rho = _nfw_dispersion ** 2 / (4 * np.pi * cgs_G * _nfw_radius ** 2)
 
-def nfwPotential(position):
-	'''
-	Calculates Navarro-Frenk-White gravitational potential at a given position 
-	in cell coordinates.
-	Note: This is slow since the looping is not numpyized. But it's a homework
-	problem so that's not particularly important
-	@param position: 3-tuple cell index to calculate potential
-	'''
-	position = np.array(position) ## Turn tuple into vector
-	total = 0.0
-	for cell in np.ndindex((61,61,61)):
-		cell = np.array(cell)
-		cellDist = physicalDist(position,cell)
-		## Only accumulate for cells that aren't the test cell
-		if(cellDist > 0.0):
-			total += nfwDensity(cell) / cellDist
-	return -cgs_G * cellSize**3 * total
 
-def nfwDensity(position):
-	'''
-	Calculates the Navarro-Frenk-White density at a given position in cell 
-	coordinates
-	@param position: 3-tuple cell index to calculate density
-	'''
-	radMag = physicalDist(position, (30,30,30))
-	if(radMag > 0.0):
-		return  nfwRho * nfwRadius / (radMag * (1 + radMag / nfwRadius)**2)
-	else:
-		return 0.0
-	
-def analyticNFWPotential(position):
-	'''
-	Calculates the Navarro-Frenk-White potential analytically
-	@param position: 3-tuple cell index to calculate potential 
-	'''
-	radMag = physicalDist(np.array(position), (30,30,30))
-	return -nfwDispersion**2 * np.log(1+radMag/nfwRadius) * nfwRadius / radMag
+def nfw_potential(position):
+    """
+    Calculates Navarro-Frenk-White gravitational potential at a given position 
+    in cell coordinates.
+    Note: This is slow since the looping is not numpyized. But it's a homework
+    problem so that's not particularly important
+    @param position: 3-tuple cell index to calculate potential
+    """
+    position = np.array(position)  # Turn tuple into vector
+    total = 0.0
+    for cell in np.ndindex((61, 61, 61)):
+        cell = np.array(cell)
+        cell_dist = physical_dist(position, cell)
+        # Only accumulate for cells that aren't the test cell
+        if cell_dist > 0.0:
+            total += nfw_density(cell) / cell_dist
+    return -cgs_G * cell_size ** 3 * total
 
-def physicalDist(cell1, cell2):
-	'''
-	Finds the physical (kpc) distance between cell1 and cell2
-	@param cell1: numpy array of first cell index
-	@param cell2: numpy array of second cell index
-	'''
-	return cellSize * norm(cell1 - cell2, 2) ## explicity use 2-norm
 
-radii = np.arange(30,61)
-potentials = np.zeros(radii.shape, dtype=float)
-analPotentials = np.zeros(radii.shape, dtype=float)
-for i,rad in enumerate(radii):
-	print 'calculating radius %i...' % rad,
-	potentials[i] = nfwPotential((rad,30,30))
-	analPotentials[i] = analyticNFWPotential((rad,30,30))
-	print '%2f (numeric) %2f (analytic)' % (potentials[i], analPotentials[i])
+def nfw_density(position):
+    """
+    Calculates the Navarro-Frenk-White density at a given position in cell 
+    coordinates
+    @param position: 3-tuple cell index to calculate density
+    """
+    rad_mag = physical_dist(position, (30, 30, 30))
+    if rad_mag > 0.0:
+        return nfw_rho * _nfw_radius \
+               / (rad_mag * (1 + rad_mag / _nfw_radius) ** 2)
+    else:
+        return 0.0
+    
 
-## Plot everything up
-#fig = pyplot.figure()
-#ax = pyplot.subplot(111)
-#ax.plot((radii-30)*cellSize,potentials, label='Numeric $\Phi$')
-#ax.plot((radii-30)*cellSize,analPotentials, label='Analytic $\Phi$')
-#ax.plot((radii-30)*cellSize,potentials-8650, label='Numeric $\Phi-8650$')
-#ax.legend()
-#pyplot.show()
+def analytic_nfw_potential(position):
+    """
+    Calculates the Navarro-Frenk-White potential analytically
+    @param position: 3-tuple cell index to calculate potential 
+    """
+    rad_mag = physical_dist(np.array(position), (30, 30, 30))
+    return -_nfw_dispersion ** 2 * np.log(1 + rad_mag / _nfw_radius) \
+           * _nfw_radius / rad_mag
+
+
+def physical_dist(cell1, cell2):
+    """
+    Finds the physical (kpc) distance between cell1 and cell2
+    @param cell1: numpy array of first cell index
+    @param cell2: numpy array of second cell index
+    """
+    return cell_size * norm(cell1 - cell2, 2)  # explicity use 2-norm
+
+
+if __name__ == '__main__':
+
+    radii = np.arange(30, 61)
+    potentials = np.zeros(radii.shape, dtype=float)
+    analytic_potentials = np.zeros(radii.shape, dtype=float)
+    for i, rad in enumerate(radii):
+        print('calculating radius %i...' % rad,)
+        potentials[i] = nfw_potential((rad, 30, 30))
+        analytic_potentials[i] = analytic_nfw_potential((rad, 30, 30))
+        print('%2f (numeric) %2f (analytic)' %
+              (potentials[i], analytic_potentials[i]))
+
+# Plot everything up
+# fig = pyplot.figure()
+# ax = pyplot.subplot(111)
+# ax.plot((radii-30)*cellSize,potentials, label='Numeric $\Phi$')
+# ax.plot((radii-30)*cellSize,analPotentials, label='Analytic $\Phi$')
+# ax.plot((radii-30)*cellSize,potentials-8650, label='Numeric $\Phi-8650$')
+# ax.legend()
+# pyplot.show()
--- AST 531 Galactic Dynamics/ass2prob5b.py
+++ AST 531 Galactic Dynamics/ass2prob5b.py
@@ -2,46 +2,50 @@ from __future__ import division
 import numpy as np
 from matplotlib import pyplot
 
-nfwDispersion = 200.0 # km/s
-nfwRadius = 5.0 # kpc
-cellSize = 0.5 # kpc
+nfw_dispersion = 200.0  # km/s
+nfw_radius = 5.0  # kpc
+cell_size = 0.5  # kpc
 
-## I split this part into a separate file for my convenience
-## Potentials, calculated from other file
-potentials = np.array([-28789.53248398, -28012.72715114, -27189.71325385, 
-					-25979.10634141, -24777.00779663, -23643.8818753,  
-					-22589.70049077, -21611.80990653, -20704.16744864, 
-					-19860.12727408, -19073.34885662, -18338.07476767,
-					-17649.17997044, -17002.1377608,  -16392.9584649,
-					-15818.12321584, -15274.52148289, -14759.39531239, 
-					-14270.29083278, -13805.01660086, -13361.60802181,
-					-12938.29700704, -12533.48608081, -12145.72623491,
-					-11773.69792794, -11416.19471723, -11072.10909245,
-					-10740.42014816, -10420.18279014, -10110.51820834,
-					-9810.60063246]) - 8650
+# I split this part into a separate file for my convenience
+# Potentials, calculated from other file
+potentials = np.array([-28789.53248398, -28012.72715114, -27189.71325385,
+                       -25979.10634141, -24777.00779663, -23643.8818753,
+                       -22589.70049077, -21611.80990653, -20704.16744864,
+                       -19860.12727408, -19073.34885662, -18338.07476767,
+                       -17649.17997044, -17002.1377608,  -16392.9584649,
+                       -15818.12321584, -15274.52148289, -14759.39531239,
+                       -14270.29083278, -13805.01660086, -13361.60802181,
+                       -12938.29700704, -12533.48608081, -12145.72623491,
+                       -11773.69792794, -11416.19471723, -11072.10909245,
+                       -10740.42014816, -10420.18279014, -10110.51820834,
+                       -9810.60063246]) - 8650
 
-def nfwRadialForce(radius):
-	radius = int(radius - 0.5)
-	return -(potentials[radius + 1] - potentials[radius])/cellSize
 
-def nfwVCirc(radius):
-	return np.sqrt(-nfwRadialForce(radius)*(radius)*cellSize)
+def nfw_radial_force(radius):
+    radius = int(radius - 0.5)
+    return -(potentials[radius + 1] - potentials[radius]) / cell_size
 
-def analyticNFWVCirc(radius):
-	radius = (radius-0.5)*cellSize ## Translate to physical units
-	return nfwDispersion*np.sqrt(np.log(1+radius/nfwRadius)/(radius/nfwRadius)
-								- 1/(1+radius/nfwRadius))
 
-radii = np.linspace(0,29,30) + 0.5
-velocities = np.zeros(radii.shape,dtype=float)
-analVelocities = np.zeros(radii.shape,dtype=float)
-for i,rad in enumerate(radii):
-	velocities[i] = nfwVCirc(rad)
-	analVelocities[i] = analyticNFWVCirc(rad)
+def nfw_v_circ(radius):
+    return np.sqrt(-nfw_radial_force(radius) * radius * cell_size)
+
+
+def analytic_nfw_v_circ(radius):
+    radius = (radius-0.5) * cell_size  # Translate to physical units
+    return nfw_dispersion * np.sqrt(np.log(1 + radius / nfw_radius)
+                                    / (radius / nfw_radius)
+                                    - 1 / (1 + radius / nfw_radius))
+
+radii = np.linspace(0, 29, 30) + 0.5
+velocities = np.zeros(radii.shape, dtype=float)
+analytic_velocities = np.zeros(radii.shape, dtype=float)
+for i, rad in enumerate(radii):
+    velocities[i] = nfw_v_circ(rad)
+    analytic_velocities[i] = analytic_nfw_v_circ(rad)
 
 fig = pyplot.figure()
 ax = pyplot.subplot(111)
-ax.plot(radii*cellSize,velocities, label='Numeric $V_c$')
-ax.plot(radii*cellSize,analVelocities, label='Analytic $V_c$')
+ax.plot(radii * cell_size, velocities, label='Numeric $V_c$')
+ax.plot(radii * cell_size, analytic_velocities, label='Analytic $V_c$')
 ax.legend()
 pyplot.show()
