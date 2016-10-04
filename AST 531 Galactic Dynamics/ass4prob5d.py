17f0d71dc308560bcdb815236e9700ee449e42ab
Matt Mechtley <mmechtley@gmail.com>
2016-10-04 15:15:45 -0400

    Made python nicer and more pythonic is assignent 4 prob 5 d.

--- AST 531 Galactic Dynamics/ass4prob5d.py
+++ AST 531 Galactic Dynamics/ass4prob5d.py
@@ -1,82 +1,88 @@
+from __future__ import division, print_function
 import numpy as np
 from matplotlib import pyplot
-from progressbar import ProgressBar
 
-stepSize = 1e-3
-steps = int(10/stepSize)
+step_size = 1e-3
+steps = int(10 / step_size)
 
 phi0 = 0.0
-velR0 = -0.2
-velZ0 = 0.2
-rad0 = 0.5
-height0 = 0.7
-q = 1.0
+vel_r0 = 0.0
+vel_z0 = 0.0
+rad0 = 0.31
+height0 = 0.17
+q = 0.9
 
-## Angular momenta of orbits we will calculate
+# Angular momenta of orbits we will calculate
 Lz = 0.2
 
-## Equation for dPhi/dt in Axisymmetric potential
-def phiDot(angMomentum, radius):
-	return angMomentum / radius**2
 
-## Equation for dv_r/dt in Axisymmetric potential
-def velRDot(angMomentum, radius, height):
-	return angMomentum**2/radius**3 - radius/(radius**2 + height**2)
+# Equation for dPhi/dt in Axisymmetric potential
+def phi_dot(ang_momentum, radius):
+    return ang_momentum / radius ** 2
 
-## Equation for dv_z/dt in Axisymmetric potential
-def velZDot(q, radius, height):
-	return -height/q/(radius**2 + height**2)
 
-## Using leapfrog integration, find velocity at t=1/2
-velRHalf = velR0 + stepSize/2.*velRDot(Lz, rad0, height0)
-velZHalf = velZ0 + stepSize/2.*velZDot(q, rad0, height0)
+# Equation for dv_r/dt in Axisymmetric potential
+def vel_r_dot(ang_momentum, radius, height):
+    return ang_momentum ** 2 / radius ** 3 \
+           - radius / (radius ** 2 + height ** 2)
 
-## Rows are timesteps, columns are values of L
+
+# Equation for dv_z/dt in Axisymmetric potential
+def vel_z_dot(q, radius, height):
+    return -height / q / (radius**2 + height**2)
+
+# Using leapfrog integration, find velocity at t=1/2
+vel_r_half = vel_r0 + step_size / 2. * vel_r_dot(Lz, rad0, height0)
+vel_z_half = vel_z0 + step_size / 2. * vel_z_dot(q, rad0, height0)
+
+# Rows are timesteps, columns are values of L
 phis = np.zeros((steps,))
 radii = np.zeros((steps,))
 heights = np.zeros((steps,))
-velrs = np.zeros((steps,))
-velzs = np.zeros((steps,))
+vel_rs = np.zeros((steps,))
+vel_zs = np.zeros((steps,))
 
-## Set initial conditions for each column (L value)
+# Set initial conditions for each column (L value)
 phis[0] = phi0
 radii[0] = rad0
 heights[0] = height0
-velzs[0] = velZHalf
-velrs[0] = velRHalf
+vel_zs[0] = vel_z_half
+vel_rs[0] = vel_r_half
 
-## Add a progress bar in the terminal so we know how far along the calculation is
-pBar = ProgressBar(maxval=len(phis.flat)).start()
+for step in range(1, steps):
+    phis[step] = phis[step-1] + step_size * phi_dot(Lz, radii[step - 1])
+    radii[step] = radii[step-1] + step_size * vel_rs[step - 1]
+    heights[step] = heights[step-1] + step_size * vel_zs[step - 1] / q
+    vel_rs[step] = vel_rs[step - 1] + step_size * vel_r_dot(
+        Lz, radii[step], heights[step])
+    vel_zs[step] = vel_zs[step - 1] + step_size * vel_z_dot(
+        q, radii[step], heights[step])
 
-for step in xrange(1,steps):
-	phis[step] = phis[step-1] + stepSize*phiDot(Lz, radii[step-1])
-	radii[step] = radii[step-1] + stepSize*velrs[step-1]
-	heights[step] = heights[step-1] + stepSize*velzs[step-1]/q
-	velrs[step] = velrs[step-1] + stepSize*velRDot(Lz, radii[step], heights[step])
-	velzs[step] = velzs[step-1] + stepSize*velZDot(q, radii[step], heights[step])
-	
-	pBar.update(step)
-		
-pBar.finish()
+    next_pct = 100 * (step + 1) // steps
+    curr_pct = 100 * step // steps
+    if next_pct - curr_pct > 0:
+        print('{:d}%'.format(next_pct))
+print('')
 
-## find zero crossings
-zcUpMask = (np.diff(np.sign(heights), 1) > 0)
+# find zero crossings
+up_zc_mask = np.diff(np.sign(heights), 1) > 0
 
-plotTitle = r'''$\~v_{r,\tau=0} = %0.2g$, $\~v_{z,\tau=0} = %0.2g$, 
-$\~r_{\tau=0} = %0.2g$, $\~z_{\tau=0} = %0.2g$''' % (velR0, velZ0, rad0, height0)
+plot_title = r'$\~v_{r,\tau=0} = %0.2g$, $\~v_{z,\tau=0} = %0.2g$,' \
+             r'$\~r_{\tau=0} = %0.2g$, $\~z_{\tau=0} = %0.2g$' % \
+             (vel_r0, vel_z0, rad0, height0)
 
-## Plot r vs. phi
+# Plot r vs. phi
 fig = pyplot.figure()
 ax = pyplot.subplot(111)
 ax.plot(radii, heights, label='$\~L_z = %0.2g$, $q = %0.2g$' % (Lz, q))
-#pyplot.xlim(0.0,0.5)
+# pyplot.xlim(0.0,0.5)
 ax.set_xlabel('$\~r$')
 ax.set_ylabel('$\~z$')
 ax.legend(loc='lower right')
-ax.set_title(plotTitle)
+ax.set_title(plot_title)
 pyplot.show()
 
-## Plot y vs. x
+# Plot y vs. x
 ax = pyplot.subplot(111, aspect='equal', adjustable='datalim')
 x = radii*np.cos(phis)
 y = radii*np.sin(phis)
@@ -84,14 +90,15 @@ ax.plot(x, y, label='$\~L_z = %0.2g$, $q = %0.2g$' % (Lz, q))
 ax.set_xlabel('$x$')
 ax.set_ylabel('$y$')
 ax.legend(loc='upper right')
-ax.set_title(plotTitle)
+ax.set_title(plot_title)
 pyplot.show()
 
-## Plot upward zero crossings, r vs. v_r
+# Plot upward zero crossings, r vs. v_r
 ax = pyplot.subplot(111)
-ax.plot(radii[zcUpMask], velrs[zcUpMask], 'o', label='$\~L_z = %0.2g$, $q = %0.2g$' % (Lz, q))
+ax.plot(radii[1:][up_zc_mask], vel_rs[1:][up_zc_mask], 'o',
+        label='$\~L_z = %0.2g$, $q = %0.2g$' % (Lz, q))
 ax.set_xlabel('$\~r$')
 ax.set_ylabel('$\~v_r$')
 ax.legend(loc='lower right')
-ax.set_title(plotTitle)
+ax.set_title(plot_title)
 pyplot.show()
